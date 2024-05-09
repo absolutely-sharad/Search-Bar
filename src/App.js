b652b7e4f53dc424e320a1d69241dcaace9a8e31
Sharad Singh Kushwaha <sharadsingh0203@gmail.com>
2024-05-09 15:08:11 +0530

    create searchbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styles from "./App.module.css"
+export default function App() {
+  const arr = [
+    "Lion",
+    "Tiger",
+    "Elephant",
+    "Giraffe",
+    "Zebra",
+    "Leopard",
+    "Wolf",
+    "Bear",
+    "Rhino",
+    "Hippo",
+    "Cheetah",
+    "Gorilla",
+    "Kangaroo",
+    "Crocodile",
+    "Panther",
+    "Jaguar",
+    "Hyena",
+    "Buffalo",
+    "Puma",
+    "Lynx"];
+  const [data, setData] = useState("");
+  const [updata, setupData] = useState([]);
+  function handleChange(e) {
+    e.preventDefault();
+    setData(e.target.value);
+    setupData(arr.filter(ele => (ele.toLowerCase()).includes(data.toLowerCase())));
+  }
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.main}>
+      <h1>SEARCH BAR</h1>
+      <form>
+        <input type='text' id={styles.input} onChange={handleChange} placeholder='Search here' />
+      </form>
+      {data.trim() === "" || data === undefined ||data.length===0? 
+      <ul>
+        {arr.map((ele, index) => (
+          <li key={index}>{ele}</li>
+        ))}
+      </ul> :
+        <ul>
+          {updata.map((ele, index) => (
+            <li key={index}>{ele}</li>
+          ))}
+        </ul>}
+
     </div>
   );
 }
-
-export default App;
